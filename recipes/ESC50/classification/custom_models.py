55a9b0e4e1061cf517ba4c0eb5ac92d054b522ab
fpaissan <me@francescopaissan.it>
2022-11-30 11:04:25 -0500

    Psi for sepdecoder conditioning

--- recipes/ESC50/classification/custom_models.py
+++ recipes/ESC50/classification/custom_models.py
@@ -4,8 +4,45 @@ import torch.nn.functional as F
 
 import speechbrain as sb
 from speechbrain.lobes.models.dual_path import (
-        Encoder, SBTransformerBlock, Dual_Path_Model, Decoder
-    )
+    Encoder,
+    SBTransformerBlock,
+    Dual_Path_Model,
+    Decoder,
+)
+
+
+class Psi(nn.Module):
+    def __init__(self, n_comp=256, in_embed_dims=[2048, 1024, 512]):
+        super().__init__()
+        """
+        Input to this Module for Cnn14 will have shape (2048, W, H), (1024, W, H)
+        and (512, 2W, 2H). For the broadcasting variant of the conditioning,
+        the decoder expects two tensors of shape (B, C, 1) -- C=256 for now.
+        """
+        self.conv_1 = nn.Conv2d(
+            in_embed_dims[2],
+            in_embed_dims[1],
+            kernel_size=3,
+            stride=2,
+            padding=1,
+        )
+        self.bn = nn.BatchNorm2d(in_embed_dims[1])
+        self.relu = nn.ReLU()
+
+        self.out = nn.Linear(in_embed_dims[0] * 2, 2 * n_comp)
+
+    def forward(self, f_i):
+        batch_size = f_i[0].shape[0]
+        # f_I is a tuple of hidden representations
+        x3 = self.relu(self.bn(self.conv_1(f_i[2])))
+
+        comb = torch.cat((x3, f_i[1], f_i[0]), dim=1)
+        comb = F.adaptive_avg_pool2d(comb, (1, 1)).view(batch_size, -1)
+
+        psi_out = self.out(comb).view(2, batch_size, -1, 1)
+
+        return psi_out
+
 
 class SepDecoder(nn.Module):
     def __init__(
@@ -67,12 +104,14 @@ class SepDecoder(nn.Module):
     def _check_shapes(x, psi_out):
         for i in range(x.dim() - 1):
             # -1 only if broadcasting in time!
-            assert x.shape[i] == psi_out.shape[i], f"{i+1}-th of input and psi_out doesn't match. Got {psi_out.shape} expect {x.shape}."
+            assert (
+                x.shape[i] == psi_out.shape[i]
+            ), f"{i+1}-th of input and psi_out doesn't match. Got {psi_out.shape} expect {x.shape}."
 
     def forward(self, x, psi_out):
         # psi_out is expected to be sth like (2, B, out_channels, T_strided)
         mix_w = self.encoder(x)
-        
+
         # check shapes before conditioning
         self._check_shapes(mix_w, psi_out[0])
         self._check_shapes(mix_w, psi_out[1])
@@ -81,9 +120,9 @@ class SepDecoder(nn.Module):
         mix_w = mix_w * psi_out[0] + psi_out[1]
 
         # generates masks for garbage and interpretation
-        est_mask = self.masknet(mix_w) 
-        
-        mix_w = torch.stack([mix_w] * 2) # stack to avoid broadcasting errors
+        est_mask = self.masknet(mix_w)
+
+        mix_w = torch.stack([mix_w] * 2)  # stack to avoid broadcasting errors
         sep_h = mix_w * est_mask
 
         # decode from latent space to time domain
@@ -101,11 +140,19 @@ class SepDecoder(nn.Module):
 
         return est_source
 
+
 if __name__ == "__main__":
     x = torch.randn(1, 16000 * 2)
-    psi_out = torch.randn(2, 1, 256, 1)
-    m = SepDecoder()
-    from torchinfo import summary
-    summary(m)
-    m(x, psi_out)
+    f_i = [
+        torch.randn(1, 2048, 6, 2),
+        torch.randn(1, 1024, 6, 2),
+        torch.randn(1, 512, 12, 4),
+    ]
+    psi = Psi()
+    psi_out = psi(f_i)
 
+    m = SepDecoder()
+    m(x, psi_out)
+    # from torchinfo import summary
+    # summary(m)
+    # m(x, psi_out)
